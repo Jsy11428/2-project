cbfb2f68ce7007d4d3117093f58bb3626724f671
composer <theboygirl@naver.com>
2024-03-06 17:25:25 +0900

    add admin create menu method

--- kioskPjt/src/main/java/com/office/kiosk/admin/menu/AdminMenuController.java
+++ kioskPjt/src/main/java/com/office/kiosk/admin/menu/AdminMenuController.java
@@ -3,6 +3,7 @@ package com.office.kiosk.admin.menu;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,10 +27,7 @@ public class AdminMenuController {
 
 	@Autowired
 	AdminMenuService adminMenuService;
-	
-	@Autowired
-	UploadFileService uploadFileService;
-	
+		
 	// 메뉴 등록 화면 페이지네이션
 	
 	@GetMapping("/createMenuForm")
@@ -57,10 +55,11 @@ public class AdminMenuController {
 		log.info(file);
 		
 		
-		String saveFileName = uploadFileService.upload(file);
+		ResponseEntity<String> saveFileName = adminMenuService.uploadFile(file);
+		
 			
 		if(saveFileName != null) {
-			adminMenuDto.setFc_menu_img_name(saveFileName); 
+			adminMenuDto.setFc_menu_img_name(saveFileName.getBody()); 
 		
 		int result =
 		adminMenuService.createMenuAccountConfirm(adminMenuDto);
