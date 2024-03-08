c9807f3e56ffb72978332d481602cd9b7c7ca781
jsy11428 <jsy11428@naver.com>
2024-03-08 19:37:45 +0900

    order list page change

--- kioskPjt/src/main/java/com/office/kiosk/franchisee/order/FranchiseeOrderController.java
+++ kioskPjt/src/main/java/com/office/kiosk/franchisee/order/FranchiseeOrderController.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.office.kiosk.franchisee.member.FranchiseeMemberDto;
+import com.office.kiosk.paging.kioskPageDto;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.log4j.Log4j2;
@@ -31,47 +33,42 @@ public class FranchiseeOrderController {
 		return nextPage;
 	}
 	
-	// 오더 리스트 다 보여주기
-//	@PostMapping("/getOrders")
-//	@ResponseBody
-//	public Object getOrders() {
-//		log.info("getOrders()");		
-//	
-//		Map<String, Object> orderDtos = franchiseeOrderService.getAllOrders();
-//		
-//		return orderDtos;
-//	}
-	
+
 	// 로그인 한 지점의 오더 리스트 보여주기
-//	@PostMapping("/getOrders")
+//	@PostMapping("/getOrdersByLogin")
 //	@ResponseBody
-//	public Object getOrders(HttpSession session) {
-//		log.info("getOrders()");		
-//		
-//	FranchiseeMemberDto loginedFranchiseeMemberDto = 
-//			(FranchiseeMemberDto) session.getAttribute("loginedFranchiseeMemberDto");
-//		
-//    int loginNo = loginedFranchiseeMemberDto.getFcm_no();
-//    
-//    Map<String, Object> orderDtos = franchiseeOrderService.getOrdersNo(loginNo);
-//	
-//		return orderDtos;
+//	public Object getOrdersByLogin(HttpSession session) {
+//	    log.info("getOrdersByLogin()");        
+//	        
+//	    FranchiseeMemberDto loginedFranchiseeMemberDto = 
+//	            (FranchiseeMemberDto) session.getAttribute("loginedFranchiseeMemberDto");
+//	    
+//	    int loginNo = loginedFranchiseeMemberDto.getFcm_no();
+//	    
+//	    Map<String, Object> orderDtos = franchiseeOrderService.getOrdersNo(loginNo);
+//	    
+//	    return orderDtos;
 //	}
 	
-	@PostMapping("/getOrdersByLogin")
+	@GetMapping("/getOrdersByLogin")
 	@ResponseBody
-	public Object getOrdersByLogin(HttpSession session) {
+	public Object getOrdersByLogin(@RequestParam(value = "page", required = false, defaultValue = "1") int page, HttpSession session) {
 	    log.info("getOrdersByLogin()");        
-	        
+	    log.info("page: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+ page);   
+	    
 	    FranchiseeMemberDto loginedFranchiseeMemberDto = 
 	            (FranchiseeMemberDto) session.getAttribute("loginedFranchiseeMemberDto");
-	    
-	    int loginNo = loginedFranchiseeMemberDto.getFcm_no();
-	    
-	    Map<String, Object> orderDtos = franchiseeOrderService.getOrdersNo(loginNo);
-	    
-	    return orderDtos;
+	      
+	    	
+	    	Map<String, Object> pagingOrderDtos = franchiseeOrderService.pagingOrderList(page);
+	    	
+	    	kioskPageDto orderListPageDto = franchiseeOrderService.getAllOrderListPageNum(page);
+	    	
+	    	pagingOrderDtos.put("orderListPageDto", orderListPageDto);
+	    	
+	    	return pagingOrderDtos;
 	}
+
 	
 	
 	
--- kioskPjt/src/main/java/com/office/kiosk/franchisee/order/IFranchiseeOrderDao.java
+++ kioskPjt/src/main/java/com/office/kiosk/franchisee/order/IFranchiseeOrderDao.java
@@ -1,14 +1,17 @@
 package com.office.kiosk.franchisee.order;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface IFranchiseeOrderDao {
 
-//	public List<FranchiseeOrderDto> selectAllOrder();
+//	public List<FranchiseeOrderDto> getOrdersByNo(int loginNo);
 
-	public List<FranchiseeOrderDto> getOrdersByNo(int loginNo);
+	public List<FranchiseeOrderDto> selectOrderPagingList(Map<String, Integer> pagingParams);
+
+	public int selectAllOrderListCnt();
 
 }
