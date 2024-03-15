1e1c6372f3451b35ce0b15608969c0d73650297c
LSY <souldkdl93@gamil.com>
2024-03-15 19:06:50 +0900

    19:06

--- kioskPjt/src/main/java/com/office/kiosk/franchisee/sales/FranchiseeSalesController.java
+++ kioskPjt/src/main/java/com/office/kiosk/franchisee/sales/FranchiseeSalesController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -48,10 +50,49 @@ public class FranchiseeSalesController {
 		kioskPageDto myStoreAllSalesPageDtos = franchiseeSalesService.getMyStoreSalesInfoPageNum(page, fcs_no);
 		
 		pagingMyStoreAllSalesDtos.put("myStoreAllSalesPageDtos", myStoreAllSalesPageDtos);
-		log.info("pagingMyStoreAllSalesDtos------------------->" + pagingMyStoreAllSalesDtos);
+		// log.info("pagingMyStoreAllSalesDtos------------------->" + pagingMyStoreAllSalesDtos);
 		
 		
 		return pagingMyStoreAllSalesDtos;
+		
+	}
+	
+	// 우리가게의 하루 매출 가져오기(년도, 월, 일 셀렉트 체인지 시)
+	
+	@PostMapping("/getCurrentDateMyStoreSalesInfo")
+	@ResponseBody
+	public Object getCurrentDateMyStoreSalesInfo(@RequestBody Map<String, String> currentDate) {
+		log.info("getCurrentDateMyStoreSalesInfo()");
+		
+		
+		 Map<String, Object> pagingMyStoreSalesInfoBySelectDate =
+		 franchiseeSalesService.pagingMyStoreSalesInfoBySelectDate(currentDate);
+		 
+		 kioskPageDto myStoreSalesInfoBySelectDatePageNum = franchiseeSalesService.getMyStoreSalesInfoBySelectDatePageNum(currentDate);
+		
+		 pagingMyStoreSalesInfoBySelectDate.put("myStoreSalesInfoBySelectDatePageNum", myStoreSalesInfoBySelectDatePageNum);
+		
+		 return pagingMyStoreSalesInfoBySelectDate;
+		
+	}
+	
+	// 우리가게의 기간별 매출 가져오기(달력선택으로 기간 선택 시)
+	
+	@PostMapping("/getMyStoreSalesInfoByInputPeriod")
+	@ResponseBody
+	public Object getMyStoreSalesInfoByInputPeriod(@RequestBody Map<String, String> period) {
+		log.info("getCurrentDateMyStoreSalesInfo()");
+		
+		 Map<String, Object> pagingMyStoreSalesInfoByInputPeriod =
+		 franchiseeSalesService.pagingMyStoreSalesInfoByInputPeriod(period);
+		 
+		 kioskPageDto myStoreSalesInfoByInputPeriodPageNum = franchiseeSalesService.getMyStoreSalesInfoByInputPeriodPageNum(period);
+		
+		 pagingMyStoreSalesInfoByInputPeriod.put("myStoreSalesInfoByInputPeriodPageNum", myStoreSalesInfoByInputPeriodPageNum);
+		 
+		 log.info("pagingMyStoreSalesInfoByInputPeriod---------?" + pagingMyStoreSalesInfoByInputPeriod);
+		
+		 return pagingMyStoreSalesInfoByInputPeriod;
 		
 	}
 	
