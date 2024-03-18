55e6ba1babb3b055d95cd424d54244aea2af1a37
jsy11428 <jsy11428@naver.com>
2024-03-18 12:37:07 +0900

    12:37

--- kioskPjt/src/main/java/com/office/kiosk/franchisee/menu/FranchiseeMenuController.java
+++ kioskPjt/src/main/java/com/office/kiosk/franchisee/menu/FranchiseeMenuController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.office.kiosk.admin.menu.AdminMenuDto;
+import com.office.kiosk.franchisee.member.FranchiseeMemberDto;
 import com.office.kiosk.paging.kioskPageDto;
 
 import ch.qos.logback.core.model.Model;
@@ -39,39 +40,39 @@ public class FranchiseeMenuController {
 //	
 	// 프랜차이즈 메뉴 추가 신청 화면
 	
-	@GetMapping("/createMenuForm")	
-	public String createMenuForm() {
-		log.info("createMenuForm()");
-		
-		String nextPage = "/franchisee/menu/create_menu_account_form";
-		
-		return nextPage;
-		
-	}
-	
+//	@GetMapping("/createMenuForm")	
+//	public String createMenuForm() {
+//		log.info("createMenuForm()");
+//		
+//		String nextPage = "/franchisee/menu/create_menu_account_form";
+//		
+//		return nextPage;
+//		
+//	}
+//	
 
 	// 프랜차이즈 메뉴 추가 컨펌
 	
-	@PostMapping("/createMenuAccountConfirm")
-	public String createMenuAccountConfirm(FranchiseeAddMenuDto franchiseeAddMenuDto, Model model) {
-		log.info("createMenuAccountConfirm()");
-		
-		String nextPage = "/franchisee/menu/create_menu_account_ok";
-		
-		int result = -1;
-		
-		result = franchiseeMenuService.createMenuAccountConfirm(franchiseeAddMenuDto);
-		
-		if (result <= 0) {
-			
-			nextPage = "/franchisee/menu/create_menu_account_ng";
-			
-		}		
-		
-		return nextPage;
-		
-	}	
-	
+//	@PostMapping("/createMenuAccountConfirm")
+//	public String createMenuAccountConfirm(FranchiseeAddMenuDto franchiseeAddMenuDto, Model model) {
+//		log.info("createMenuAccountConfirm()");
+//		
+//		String nextPage = "/franchisee/menu/create_menu_account_ok";
+//		
+//		int result = -1;
+//		
+//		result = franchiseeMenuService.createMenuAccountConfirm(franchiseeAddMenuDto);
+//		
+//		if (result <= 0) {
+//			
+//			nextPage = "/franchisee/menu/create_menu_account_ng";
+//			
+//		}		
+//		
+//		return nextPage;
+//		
+//	}	
+//	
 	
 	// 프랜차이즈 메뉴 리스트(전체) 화면
 	
@@ -79,7 +80,7 @@ public class FranchiseeMenuController {
 //	public Object getAllMenus(HttpSession session, Map<String, Object> menuMap) {
 		public Object getAllMenus() {
 		log.info("getAllMenus()");
-		
+				
 		String nextPage = "/franchisee/menu/franchisee_menu_list";
 		
 		
@@ -142,26 +143,26 @@ public class FranchiseeMenuController {
 	
 	// 프랜차이즈 메뉴 수정 화면
 	
-	@GetMapping("/modifySelectMenu")
-	public String modifySelectMenu(Model model) {
-		log.info("modifySelectMenu()");
-		
-		String nextPage = "/franchisee/menu/modify_menu_account_form";
-		
-		return nextPage;
-		
-	}
-	
-	@PostMapping("/getSelectMenuInfo")
-	@ResponseBody
-	public FranchiseeMenuDto getSelectMenuInfo(Model model, @RequestParam("fc_menu_no") String fc_menu_no) {
-		log.info("getSelectMenuInfo()");
-
-		FranchiseeMenuDto dto = franchiseeMenuService.getSelectMenuInfo(fc_menu_no);
-
-		return dto;
-	}
-	
+//	@GetMapping("/modifySelectMenu")
+//	public String modifySelectMenu(Model model) {
+//		log.info("modifySelectMenu()");
+//		
+//		String nextPage = "/franchisee/menu/modify_menu_account_form";
+//		
+//		return nextPage;
+//		
+//	}
+//	
+//	@PostMapping("/getSelectMenuInfo")
+//	@ResponseBody
+//	public FranchiseeMenuDto getSelectMenuInfo(Model model, @RequestParam("fc_menu_no") String fc_menu_no) {
+//		log.info("getSelectMenuInfo()");
+//
+//		FranchiseeMenuDto dto = franchiseeMenuService.getSelectMenuInfo(fc_menu_no);
+//
+//		return dto;
+//	}
+//	
 	
 	// 프랜차이즈 메뉴 수정 컨펌
 	
